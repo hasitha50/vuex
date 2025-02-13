ad13da588b9e3fe9d3b13cc24643ba09cf282286
hasitha.herath <hasitha.herath@qualitapps.com>
2025-02-13 15:35:52 +0530

    1st commit

---
+++ Firebase_Auth/README.md
@@ -0,0 +1,14 @@
+# vuex 
+
+## Project setup
+```
+npm install
+```
+
+### setup env
+
+
+### Compiles and minifies for production
+```
+npm run serve
+```
---
+++ Firebase_Auth/src/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: process.env.VITE_FIREBASE_API_KEY,
+    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_FIREBASE_APP_ID,
+    measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
+  };
+  
+  initializeApp(firebaseConfig)
+
+  const auth =getAuth()
+
+  export {auth};
